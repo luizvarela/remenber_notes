4554fb9c46ff55c0da9b17bda285b88ed567cac7
luizvarela <luiz.lvarela@gmail.com>
2013-03-02 22:49:00 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server);
+
+app.configure(function() {
+	app.use(express.static(__dirname + '/public'));
+});
+
+io.sockets.on('connection', function(socket){
+	socket.on('createNote', function(data) {
+		socket.broadcast.emit('onNoteCreated',data);
+	});
+
+	socket.on('updateNote', function(data) {
+		socket.broadcast.emit('onNoteUpdated',data);
+	});
+
+	socket.on('deleteNote', function(data) {
+		socket.broadcast.emit('onNoteDeleted',data);
+	});
+
+	socket.on('moveNote', function(data) {
+		socket.broadcast.emit('onNoteMoved',data);
+	});
+
+});
+
+server.listen(1337);
